b774e0324a7b295ec23adc192a00ace88a839497
simonakesson <simonakesson@hotmail.com>
2019-08-25 19:14:23 +0200

    Bug roadblock

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include "threadpool.hpp"
 
+#include <chrono>
+#include <thread>
 
 int main() {
-    ThreadPool pool;
+    ThreadPool pool(2);
+    Task task;
+    Task task1;
+    Task task2;
+    Task task3;
+    pool.push(task);
+    // pool.push(task1);
+    // pool.push(task2);
+    // pool.push(task3);
+    // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     pool.join();
 }
--- threadpool.hpp
+++ threadpool.hpp
@@ -5,63 +5,60 @@
 #include <condition_variable>
 #include <functional>
 
+#include <iostream>  // temp for debugging
+
 
 //template <typename T>
 class Task {
 public:
-    //Task(const std::function<T>& function) {
-
-    //}
-private:
-    //std::function<T>
+    void invoke() {
+        std::cout << "* Running task *" << std::endl;
+    }
 };
 
 
 class Worker {
 public:
-    Worker(std::queue<Task>& a_tasks, std::mutex& a_mutex, std::condition_variable& a_cond)
-        : m_tasks(a_tasks), m_mutex(a_mutex), m_cond(a_cond), m_stop(false), m_thread([this](){ run(); }) {}
+    Worker(std::queue<Task>& a_tasks, std::mutex& a_mutex, std::condition_variable& a_cond, const bool& a_stop)
+        : m_tasks(a_tasks), m_mutex(a_mutex), m_cond(a_cond), m_stop(a_stop), m_thread([this]{ run(); }) {}
     
     Worker(Worker&& rhs)
         : m_tasks(rhs.m_tasks), m_mutex(rhs.m_mutex), m_cond(rhs.m_cond), m_stop(rhs.m_stop), m_thread(std::move(rhs.m_thread)) {}
 
     void join() {
-        m_stop = true;
         m_thread.join();
     }
 
     ~Worker() {
-        join();
+        if (m_thread.joinable()) {
+            join();
+        }
     }
 
 private:
-    Task pop_task() {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_cond.wait(lock, [&]{ return !m_tasks.empty() || m_stop; });
-        if (m_stop && m_tasks.empty()) {
-            lock.unlock();
-            m_cond.notify_one();
-            return Task();  // TODO
-        } else {
+    void run() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_cond.wait(lock, [this]{ return !m_tasks.empty() || m_stop; });
+            
+            if (m_stop && m_tasks.empty()) {
+                lock.unlock();
+                m_cond.notify_one();
+                return;
+            }
+        
             Task task = std::move(m_tasks.front());
             m_tasks.pop();
             lock.unlock();
             m_cond.notify_one();
-            return std::move(task);
-        }
-    }
-
-    void run() {
-        while (!m_stop) {
-            Task task = pop_task();
-            /* Invoke task */
+            task.invoke();
         }
     }
 
     std::queue<Task>& m_tasks;
-    std::mutex& m_mutex;  // TODO: bake into queue (create thread-safe queue)
+    std::mutex& m_mutex;
     std::condition_variable& m_cond;
-    bool m_stop;
+    const bool& m_stop;
     std::thread m_thread;
 };
 
@@ -70,11 +67,18 @@ class ThreadPool {
 public:
     explicit ThreadPool(size_t n_threads = std::thread::hardware_concurrency()) {
         for (size_t i = 0; i < n_threads; ++i) {
-            m_workers.emplace_back(m_tasks, m_mutex, m_cond);
+            m_workers.emplace_back(m_tasks, m_mutex, m_cond, m_stop);
         }
     }
 
+    void push(Task task) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_tasks.push(task);
+    }
+
     void join() {
+        m_stop = true;
+        m_cond.notify_all();
         for (Worker& w : m_workers) {
             w.join();
         }
@@ -84,5 +88,6 @@ private:
     std::queue<Task> m_tasks;
     std::mutex m_mutex;
     std::condition_variable m_cond;
+    bool m_stop = false;
     std::vector<Worker> m_workers;
 };
